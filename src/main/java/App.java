179971e784610afac03887709aa5f35abfb6d27d
Eylem Kömürcü <eylemkomurcu@MacBook-Air-von-Eylem.local>
2025-06-09 22:32:40 +0200

    "GPLv3-Lizenzheader zum Quellcode in App.java hinzugefügt"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,14 @@
- public class App {
+/*
+ * Copyright (C) 2025 Eylem Kömürcü
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Dieses Programm ist freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation
+ * veröffentlicht, Version 3 oder (nach Ihrer Option) jeder späteren Version weitergeben und/oder modifizieren.
+ */
+
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
